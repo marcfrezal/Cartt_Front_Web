dfb52f95d2fe8fa702cfff1ddd592a5b16382b44
Paul Vitry <paul.vitry@weeakt.com>
2021-02-18 18:36:13 -0800

    fix sales hjistory css

--- src/components/SalesHistory/SalesHistory.js
+++ src/components/SalesHistory/SalesHistory.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-// import {StyleSheet} from 'react'
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './SalesHistory.css';
 
 var transactions = [{name: 'Paul Vitry', date:'27/02/2020', amount: 1235},
                     {name: 'Paul Vitry', date:'27/02/2020', amount: 1235},
@@ -14,19 +13,17 @@ var transactions = [{name: 'Paul Vitry', date:'27/02/2020', amount: 1235},
 
 function getItem (data) {
   return (
-      <Container style={{backgroundColor: '#C4C4C4', marginBottom: 5, height: 55, borderRadius: 5}} fluid>
-        <Row style={{justifyContent:'center', alignItems: 'center',}}>
-          <Col style={{flex:3, alignSelf: 'center',  fontSize:20}}>{data.name}</Col>
-          <Col style={{flex:3,fontSize: 20}}>{data.date}</Col>
-          <Col style={{flex:1,fontSize: 20, alignContent:'flex-end'}}>{data.amount}€</Col>
+      <Container className='itemContainer' fluid>
+        <Row>
+          <Col className='text'>{data.name}</Col>
+          <Col className='text'>{data.date}</Col>
+          <Col className='text'>{data.amount}€</Col>
         </Row>
       </Container>  
   )
 }
 
 class SalesHistory extends React.Component {
-
-    
 
     render() {
       return (
@@ -41,15 +38,6 @@ class SalesHistory extends React.Component {
       );
     }
   }
-
-  // styles = StyleSheet.create({
-  //   item: {
-
-  //   },
-  // })
-  // function fetchTransactions() {
-  //   response = fetch("").then()
-  // }
 
 export default SalesHistory;
 
