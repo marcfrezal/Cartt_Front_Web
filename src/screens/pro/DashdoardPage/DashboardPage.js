0ffe8c27fc62622cdd5942c24cbe4d98c8a72f20
Marc Frezal <marc.frezal@epitech.eu>
2021-02-23 10:42:47 +0100

    MF : code : reorganisation de l'archi, sidebar debut

--- src/screens/pro/DashdoardPage/DashboardPage.js
+++ src/screens/pro/DashdoardPage/DashboardPage.js
@@ -2,16 +2,12 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-<<<<<<< HEAD
 import SalesHistory from '../../../components/pro/SalesHistory/SalesHistory';
 import Sidebar from '../../../components/common/Sidebar/Sidebar';
-=======
-import SalesHistory from '../../../components/SalesHistory/SalesHistory'
 import MounthlyNumbers from '../../../components/MounthlyNumbers/MounthlyNumbers'
 import LastTransactions from '../../../components/LastTransactions/LastTransactions'
 import Growth from '../../../components/Growth/Growth'
 // import HeadBand from "./HeadBand/HeadBand";
->>>>>>> f6640211303cd63e144976a08f36bf79d0beb5a4
 import './DashboardPage.css'
 
 class Dashboard extends React.Component {
